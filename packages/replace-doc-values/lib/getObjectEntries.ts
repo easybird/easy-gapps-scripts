b8f8736ec2885dba5038637624d163894c2921f6
Jonathan Dierckens <jonathan.dierckens@inthepocket.mobi>
2018-12-09 09:49:10 +0100

    include getObjectEntries in the ts file to quick fix ts2gas non-recursive transpilation

--- packages/replace-doc-values/lib/getObjectEntries.ts
+++
@@ -1,9 +0,0 @@
-export default function( obj: object ){
-    var ownProps = Object.keys( obj ),
-        i = ownProps.length,
-        resArray = new Array(i); // preallocate the Array
-    while (i--)
-      resArray[i] = [ownProps[i], obj[ownProps[i]]];
-
-    return resArray;
-  }
--- packages/replace-doc-values/lib/index.ts
+++ packages/replace-doc-values/lib/index.ts
@@ -1,23 +1,35 @@
-import getObjectEntries from "./getObjectEntries";
+function getObjectEntries(obj: object) {
+  var ownProps = Object.keys(obj),
+    i = ownProps.length,
+    resArray = new Array(i); // preallocate the Array
+  while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+  return resArray;
+}
 
 interface ValueIdentifiers {
-    valuePrefix?: string;
-    valueSuffix?: string;
+  valuePrefix?: string;
+  valueSuffix?: string;
 }
-export default function replaceDocValues(documentId:string, documentData: object, valueIdentifier?: ValueIdentifiers) {
-    const doc = DocumentApp.openById(documentId)
-    const documentBody = doc.getBody();
+export default function replaceDocValues(
+  documentId: string,
+  documentData: object,
+  valueIdentifier?: ValueIdentifiers
+) {
+  const doc = DocumentApp.openById(documentId);
+  const documentBody = doc.getBody();
 
-    function addValueIdentifiersToKey(key) {
-        if (!valueIdentifier) {
-            return key;
-        }
-        return `${valueIdentifier.valuePrefix || ''}${key}${valueIdentifier.valueSuffix|| ''}`;
+  function addValueIdentifiersToKey(key) {
+    if (!valueIdentifier) {
+      return key;
     }
-
-    getObjectEntries(documentData).forEach(([key, value]) => {
-        documentBody.replaceText(addValueIdentifiersToKey(key), value);
-    })
-
-    doc.saveAndClose()
+    return `${valueIdentifier.valuePrefix ||
+      ''}${key}${valueIdentifier.valueSuffix || ''}`;
   }
+
+  getObjectEntries(documentData).forEach(([key, value]) => {
+    documentBody.replaceText(addValueIdentifiersToKey(key), value);
+  });
+
+  doc.saveAndClose();
+}
